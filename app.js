ccb933a8098a5f1fd28520679b4e962f1b64b318
prasanta1987 <prasanta.1987@hotmail.com>
2023-04-09 13:02:09 +0530

    cookie added

--- app.js
+++ app.js
@@ -7,115 +7,139 @@ const app = express()
 
 
 
-app.get('/:symbol/:fromDate/:toDate', (req, res) => {
+// app.get('/:symbol/:fromDate/:toDate', (req, res) => {
 
-    let symb = (req.params.symbol).toLowerCase()
-    symb = symb.replace('&', '%26')
-    let fromDate = req.params.fromDate
-    let toDate = req.params.toDate
+//     let symb = (req.params.symbol).toLowerCase()
+//     symb = symb.replace('&', '%26')
+//     let fromDate = req.params.fromDate
+//     let toDate = req.params.toDate
 
-    const symbolCountUrl = `https://www1.nseindia.com/marketinfo/sym_map/symbolCount.jsp?symbol=${symb}`
-
-
-    axios.get(symbolCountUrl)
-        .then(data => {
-
-            let symbolCount = data.data
-            // Det format DD-MM-YYYY
-            const url = `https://www1.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?symbol=${symb}&segmentLink=3&symbolCount=${symbolCount}&series=EQ&dateRange=+&fromDate=${fromDate}&toDate=${toDate}&dataType=PRICEVOLUMEDELIVERABLE`
-
-            axios.get(url, {
-                headers: {
-                    "Sec-Fetch-Mode": "cors",
-                    "Sec-Fetch-Dest": "empty",
-                    "Referer": "https://www1.nseindia.com/products/content/equities/equities/eq_security.htm",
-                    "Host": "www1.nseindia.com",
-                    "Connection": "keep-alive",
-                    "Accept-Language": "en-US,en;q=0.9,bn;q=0.8",
-                    "Accept-Encoding": "gzip, deflate, br",
-                    "Accept": "*/*",
-                    "Sec-Fetch-Site": "same-origin",
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
-                    "X-Requested-With": "XMLHttpRequest"
-
-                }
-            })
-                .then(data => {
-
-                    let htmlData = HTMLParser.parse(data.data).querySelector('#csvContentDiv').rawText
-                    let arrayData = htmlData.split(':')
-
-                    let headerData = arrayData.shift()
-
-                    let newData = []
-
-                    arrayData.map(values => newData.push(values.split(',')))
+//     const symbolCountUrl = `https://www1.nseindia.com/marketinfo/sym_map/symbolCount.jsp?symbol=${symb}`
 
 
-                    filteredData = []
+//     axios.get(symbolCountUrl)
+//         .then(data => {
 
-                    newData.map(x => {
-                        x = x.map(y => y.replace(/"/gi, ''))
-                        x = x.map(y => y.trim())
-                        filteredData.push(x)
-                    })
+//             let symbolCount = data.data
+//             // Det format DD-MM-YYYY
+//             const url = `https://www1.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?symbol=${symb}&segmentLink=3&symbolCount=${symbolCount}&series=EQ&dateRange=+&fromDate=${fromDate}&toDate=${toDate}&dataType=PRICEVOLUMEDELIVERABLE`
 
-                    filteredData.pop()
+//             axios.get(url, {
+//                 headers: {
+//                     "Sec-Fetch-Mode": "cors",
+//                     "Sec-Fetch-Dest": "empty",
+//                     "Referer": "https://www1.nseindia.com/products/content/equities/equities/eq_security.htm",
+//                     "Host": "www1.nseindia.com",
+//                     "Connection": "keep-alive",
+//                     "Accept-Language": "en-US,en;q=0.9,bn;q=0.8",
+//                     "Accept-Encoding": "gzip, deflate, br",
+//                     "Accept": "*/*",
+//                     "Sec-Fetch-Site": "same-origin",
+//                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+//                     "X-Requested-With": "XMLHttpRequest"
+
+//                 }
+//             })
+//                 .then(data => {
+
+//                     let htmlData = HTMLParser.parse(data.data).querySelector('#csvContentDiv').rawText
+//                     let arrayData = htmlData.split(':')
+
+//                     let headerData = arrayData.shift()
+
+//                     let newData = []
+
+//                     arrayData.map(values => newData.push(values.split(',')))
 
 
-                    arrayJsonData = []
+//                     filteredData = []
 
-                    filteredData.map(x => {
+//                     newData.map(x => {
+//                         x = x.map(y => y.replace(/"/gi, ''))
+//                         x = x.map(y => y.trim())
+//                         filteredData.push(x)
+//                     })
 
-                        let data = {
-                            symbol: x[0],
-                            series: x[1],
-                            date: x[2],
-                            preClose: x[3],
-                            openPrice: x[4],
-                            highPrice: x[5],
-                            lowPrice: x[6],
-                            lastPrice: x[7],
-                            closePrice: x[8],
-                            vwap: x[9],
-                            ttq: x[10],
-                            turnOver: x[11],
-                            noOfTrade: x[12],
-                            deliQty: x[13],
-                            dliPercen: x[14]
-                        }
-                        arrayJsonData.push(data)
+//                     filteredData.pop()
 
-                    })
 
-                    res.status(200).json(arrayJsonData)
+//                     arrayJsonData = []
 
-                })
-                .catch(err => {
-                    res.status(500).json({ "error": "Query Must Be Within a Year" })
-                })
-        })
-        .catch(err => console.log(err))
-})
+//                     filteredData.map(x => {
 
-app.get('/:symbol', (req, res) => {
+//                         let data = {
+//                             symbol: x[0],
+//                             series: x[1],
+//                             date: x[2],
+//                             preClose: x[3],
+//                             openPrice: x[4],
+//                             highPrice: x[5],
+//                             lowPrice: x[6],
+//                             lastPrice: x[7],
+//                             closePrice: x[8],
+//                             vwap: x[9],
+//                             ttq: x[10],
+//                             turnOver: x[11],
+//                             noOfTrade: x[12],
+//                             deliQty: x[13],
+//                             dliPercen: x[14]
+//                         }
+//                         arrayJsonData.push(data)
 
-    let symbolData = JSON.stringify({
-        "symbol": (req.params.symbol).toLowerCase()
-    });
+//                     })
 
+//                     res.status(200).json(arrayJsonData)
+
+//                 })
+//                 .catch(err => {
+//                     res.status(500).json({ "error": "Query Must Be Within a Year" })
+//                 })
+//         })
+//         .catch(err => console.log(err))
+// })
+
+// app.get('/:symbol', (req, res) => {
+
+//     let symbolData = JSON.stringify({
+//         "symbol": (req.params.symbol).toLowerCase()
+//     });
+
+//     var config = {
+//         method: 'post',
+//         url: 'https://api.niftytrader.in/webapi/Live/stockAnalysis',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         data: symbolData
+//     };
+
+//     axios(config)
+//         .then(response => {
+//             res.status(200).json(response.data);
+//         })
+//         .catch(function (error) {
+//             res.status(501).json(error);
+//             // console.log(error);
+//         });
+
+// })
+
+app.get('/marketStatus', (req, res) => {
     var config = {
-        method: 'post',
-        url: 'https://api.niftytrader.in/webapi/Live/stockAnalysis',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data: symbolData
+        method: 'get',
+        // url: 'https://www.nseindia.com/market-data/live-equity-market?symbol=NIFTY%2050',
+        url: 'https://www.nseindia.com/api/marketStatus',
+        // url: 'https://www.nseindia.com/',
+        // headers: {
+        //     'Content-Type': 'application/json'
+        // },
+        // data: symbolData
     };
 
     axios(config)
         .then(response => {
             res.status(200).json(response.data);
+            console.log(response.data)
         })
         .catch(function (error) {
             res.status(501).json(error);
@@ -124,6 +148,22 @@ app.get('/:symbol', (req, res) => {
 
 })
 
+
+const getCookie = () => {
+
+    var config = {
+        method: 'get',
+        url: 'https://www.nseindia.com/',
+    };
+
+    axios(config)
+        .then(response => console.log(response.headers))
+        .catch(error => console.log(error));
+
+
+}
+
+setInterval(getCookie, 5000);
 
 const port = process.env.PORT || 3000
 
