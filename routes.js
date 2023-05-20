a17a8533b1ea8025b386aef26c8edb6641969165
prasanta1987 <prasanta.1987@hotmail.com>
2023-05-20 23:55:20 +0530

    CLEANED

--- routes.js
+++ routes.js
@@ -26,32 +26,9 @@ exports.getSpotData = async (req, res) => {
     res.status(200).json(jsonData)
 }
 
-exports.marketStatus = (req, res) => {
-    const url = 'https://www.nseindia.com/api/marketStatus'
-    sendHttpRequest(req, res, url)
-}
-
 exports.landingPage = (req, res) => {
     const url = 'https://appfeeds.moneycontrol.com/jsonapi/fno/overview&format=json&inst_type=options&option_type=CE&id=NIFTY'
     sendHttpRequest(req, res, url)
 }
-
-exports.historicalData = (req, res) => {
-    const url = 'https://www.nseindia.com/api/historical/fo/derivatives/meta?&from=09-03-2023&to=09-04-2023&instrumentType=FUTIDX&symbol=NIFTY&CSV=TRUE'
-    sendHttpRequest(req, res, url)
-}
-
-exports.stockData = (req, res) => {
-    const scripCode = (req.params.symbol).toUpperCase();
-    const url = `https://www.nseindia.com/api/quote-equity?symbol=${scripCode}`
-
-    sendHttpRequest(req, res, url)
-}
-
-
-// https://www.nseindia.com/api/search/autocomplete?q=abb
-// https://www.nseindia.com/api/equity-meta-info?symbol=ABBOTINDIA
-// https://www.nseindia.com/api/quote-equity?symbol=ABBOTINDIA&section=trade_info
-// https://www.nseindia.com/api/quote-equity?symbol=ABBOTINDIA
 
 
