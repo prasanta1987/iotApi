45e0fb506f6aaf9d4aecc660ff56d0392f321c6a
prasanta1987 <prasanta.1987@hotmail.com>
2023-06-04 00:38:58 +0530

    ws removed

--- app.js
+++ app.js
@@ -1,35 +1,16 @@
+const express = require('express');
+
 const { fnoDataFetch, landingPage, search, getSpotData } = require('./routes');
 
-
-const express = require('express');
 const app = express()
-const server = require('http').createServer(app);
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ server: server });
 
-
-wss.on('connection', function connection(ws) {
-    console.log('A new client Connected!');
-    ws.send('Welcome New Client!');
-
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-
-    });
-});
+app.get('/all/:script/:data', fnoDataFetch)
+app.get('/all/:script/', getSpotData)
+app.get('/search/:script', search)
 
 
 
 app.get('/', landingPage)
-app.get('/all/:script/:data', fnoDataFetch)
-app.get('/all/:script/', getSpotData)
-app.get('/search/:script', search)
 
 
 // app.get('/marketStatus', getCookie, marketStatus)
@@ -39,4 +20,4 @@ app.get('/search/:script', search)
 
 const port = process.env.PORT || 3000
 
-server.listen(port, () => console.log(`Server Running at http://localhost:${port}`))
+app.listen(port, () => console.log(`Server Running at http://localhost:${port}`))
