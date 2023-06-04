d31e8b76ec332c3d65b2058982bd8c955f09b52e
prasanta1987 <prasanta.1987@hotmail.com>
2023-06-04 12:40:06 +0530

    upp

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const io = new Server(server);
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    io.send("Hello")
 });
 
 // const server = require('http').createServer(app);
