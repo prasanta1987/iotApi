813b7bf22be9e84469c9b1c1dbe9f4b9befb347d
prasanta1987 <prasanta.1987@hotmail.com>
2023-06-06 22:59:33 +0530

    json db added

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const express = require('express');
+const path = require("path");
+const fs = require('fs')
 
 const { fnoDataFetch, landingPage, search, getSpotData } = require('./routes');
 
 const app = express()
+const port = process.env.PORT || 3000
 
+const userProfileFile = path.join(__dirname, './user_profile/userProfile.json')
+
+fs.exists(userProfileFile, (res) => {
+    if (!res) {
+        try {
+            fs.mkdirSync(path.join(__dirname, './user_profile'))
+        } catch (error) {
+            console.log('Directory Exist')
+        } finally {
+            fs.writeFileSync(userProfileFile,
+                JSON.stringify({
+                    name: null,
+                }, null, 3)
+            )
+        }
+    }
+})
+
+app.get('/', landingPage)
 app.get('/all/:script/:data', fnoDataFetch)
 app.get('/all/:script/', getSpotData)
 app.get('/search/:script', search)
-
-
-
-app.get('/', landingPage)
 
 
 // app.get('/marketStatus', getCookie, marketStatus)
@@ -18,6 +36,5 @@ app.get('/', landingPage)
 // app.get('/symbol/:symbol', getCookie, stockData)
 
 
-const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Server Running at http://localhost:${port}`))
