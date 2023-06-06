684cafa5601b5bc6c13c860a0ae5668b394596be
prasanta1987 <prasanta.1987@hotmail.com>
2023-06-06 23:01:07 +0530

    json db removed

--- app.js
+++ app.js
@@ -7,24 +7,6 @@ const { fnoDataFetch, landingPage, search, getSpotData } = require('./routes');
 const app = express()
 const port = process.env.PORT || 3000
 
-const userProfileFile = path.join(__dirname, './user_profile/userProfile.json')
-
-fs.exists(userProfileFile, (res) => {
-    if (!res) {
-        try {
-            fs.mkdirSync(path.join(__dirname, './user_profile'))
-        } catch (error) {
-            console.log('Directory Exist')
-        } finally {
-            fs.writeFileSync(userProfileFile,
-                JSON.stringify({
-                    name: null,
-                }, null, 3)
-            )
-        }
-    }
-})
-
 app.get('/', landingPage)
 app.get('/all/:script/:data', fnoDataFetch)
 app.get('/all/:script/', getSpotData)
