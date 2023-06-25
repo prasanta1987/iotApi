eee64dc3f698d1964bea5df3b6ef8ddd27bd2df6
prasanta.1987@hotmail.com <prasanta.1987@hotmail.com>
2023-06-25 11:13:18 +0530

    hh

--- app.js
+++ app.js
@@ -85,9 +85,6 @@ app.get('/', isLogedIn, (req, res) => res.sendFile(__dirname + '/public/index.ht
 
 // Sign-In Sign-Up Handler
 app.post('/loginStatus', (req, res) => {
-  if(req.session.logedIn.logInStat == true){
-    req.session.logInStat = false
-  }
     res.status(200).json({ "logInStat": req.session.logedIn })
 })
 app.post('/login', login)
