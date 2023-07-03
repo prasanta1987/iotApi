09e352c0d3b85066c23b4129d43a6d63a1e5a9df
Prasanta Karmakar <prasanta.1987@hotmail.com>
2023-07-03 11:11:37 +0000

    chnage cord

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 var corsOptions = {
-    origin: 'https://investobaba.web.app/',
+    origin: 'https://investobaba.web.app',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
