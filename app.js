0269e901a4e5965f35269ec9c5996a4b65883499
prasanta1987 <prasanta.1987@hotmail.com>
2023-11-14 22:20:02 +0530

    upd

--- app.js
+++ app.js
@@ -107,10 +107,12 @@ app.get('/updatePics/:fileId/:tags', updatePic)
 app.post('/pingTest', (req, res) => {
 
     console.log(req.body)
+
     res.status(200).json({
         "msg": "Success"
     })
 })
+
 
 
 // Page Navigation
