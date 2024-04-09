d38d403dd809914b9fb003e5956eacdb54da2b98
Prasanta Karmakar <prasanta.1987@hotmail.com>
2024-04-09 11:46:09 +0530

    Update firebaseFunctions.js

--- firebaseFunctions.js
+++ firebaseFunctions.js
@@ -152,8 +152,8 @@ exports.getPicUrl = async (req, res) => {
   console.log(photoUrls)
 
   const randomNumber = randomIntFromInterval(0, photoUrls.length - 1)
-  let time = ((await this.getTime()).time).replace(":", "%3a")
-  let amPM = (await this.getTime()).amPM
+  let time = ((await getTime()).time).replace(":", "%3a")
+  let amPM = (await getTime()).amPM
 
 
   let currentImageUrl = `${photoUrls[randomNumber].url}/tr:w-320,h-240,l-text,ly-195,pa-5,w-320,bg-00000060,i-${time},fs-50,co-FFFFFF,ia-left,l-end:l-text,lx-130,ly-200,i-${amPM},fs-20,co-FFFFFF,l-end`
