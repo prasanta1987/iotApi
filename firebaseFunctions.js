d216e322014b1fe91103bc52c7efe2a0172c22d5
Prasanta Karmakar <prasanta.1987@hotmail.com>
2024-09-12 19:47:32 +0530

    Update firebaseFunctions.js

--- firebaseFunctions.js
+++ firebaseFunctions.js
@@ -159,7 +159,7 @@ exports.getPicUrl = async (req, res) => {
 
   // let currentImageUrl = photoUrls[randomNumber].url
 
-  let currentImageUrl = `${photoUrls[randomNumber].url}&tr=w-320,h-240,l-text,ly-195,pa-5,w-320,bg-00000060,i-${time},fs-50,co-FFFFFF,ia-left,l-end:l-text,lx-130,ly-200,i-${amPM},fs-20,co-FFFFFF,l-end`
+  let currentImageUrl = `${photoUrls[randomNumber].url}&tr=w-320,h-240`
 
   //  let currentImageUrl = `${photoUrls[randomNumber].url}/tr:w-320,h-240,l-text,ly-205,pa-5,w-320,bg-00000060,i-${time},fs-36,co-FFFFFF,ia-left,l-end:l-text,lx-100,ly-212,i-${amPM},fs-16,co-FFFFFF,l-end`
   // let currentImageUrl = `${photoUrls[randomNumber].url}/tr:w-320,h-240,l-text,ly-207,pa-5,w-320,bg-00000060,i-${time},fs-32,co-FFFFFF,ia-left,l-end:l-text,lx-90,ly-212,i-${amPM},fs-16,co-FFFFFF,l-end`
