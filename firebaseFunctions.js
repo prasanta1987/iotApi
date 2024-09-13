cc3c544d1376ae788266a33e351fe98188dfcb29
Prasanta Karmakar <prasanta.1987@hotmail.com>
2024-09-13 12:17:10 +0530

    Update firebaseFunctions.js

--- firebaseFunctions.js
+++ firebaseFunctions.js
@@ -153,7 +153,7 @@ exports.getPicUrl = async (req, res) => {
   console.log(photoUrls)
 
   const randomNumber = randomIntFromInterval(0, photoUrls.length - 1)
-  let time = ((await getTime()).time).replace(":","∞")
+  let time = ((await getTime()).time).replace(":","•")
   let amPM = (await getTime()).amPM
 
 
