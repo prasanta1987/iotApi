57a190e6b65e2e12d687566ba765e4e3edd1c9e2
Prasanta Karmakar <prasanta.1987@hotmail.com>
2025-06-20 16:11:57 +0530

    Update firebaseFunctions.js

--- firebaseFunctions.js
+++ firebaseFunctions.js
@@ -163,7 +163,7 @@ exports.getPicUrl = async (req, res) => {
   let encodedTime = btoa(time);
   console.log(encodedTime)
 
-  let currentImageUrl = `${photoUrls[randomNumber].url}/tr:w-320,h-240,l-text,ly-195,pa-5,w-320,bg-00000060,i-${encodedTime},fs-50,co-FFFFFF,ia-left,l-end:l-text,lx-130,ly-200,i-${amPM},fs-20,co-FFFFFF,l-end`
+  let currentImageUrl = `${photoUrls[randomNumber].url}/tr:w-320,h-240,l-text,ly-195,pa-5,w-320,bg-00000060,ie-${encodedTime},fs-50,co-FFFFFF,ia-left,l-end:l-text,lx-130,ly-200,i-${amPM},fs-20,co-FFFFFF,l-end`
   
   // let currentImageUrl = `${photoUrls[randomNumber].url}/tr:w-320,h-240,l-text,ly-195,pa-5,w-320,bg-00000060,i-${time},fs-50,co-FFFFFF,ia-left,l-end:l-text,lx-130,ly-200,i-${amPM},fs-20,co-FFFFFF,l-end`
   // let currentImageUrl =photoUrls[randomNumber].url
